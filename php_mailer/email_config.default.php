b50a978770c14a8c48be3bf93cfd3a6f9dbe8947
dburns1 <patriciadburns@gmail.com>
2018-07-30 12:43:59 -0700

    changed contact form added different contact information

--- php_mailer/email_config.default.php
+++ php_mailer/email_config.default.php
@@ -1,7 +1,7 @@
 <?php
-define('EMAIL_USER','<email address>');
+define('EMAIL_USER','patriciadburnscodelikeagirl@gmail.com');
 define('EMAIL_USERNAME','<name>');
-define('EMAIL_PASS','<password>');
-define('EMAIL_TO_ADDRESS','<email to send to>');
+define('EMAIL_PASS','Dovalis87!');
+define('EMAIL_TO_ADDRESS','<patriciadburnscodelikeagirl@gmail.com');
 ?>
 
